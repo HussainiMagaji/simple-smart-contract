3a30688ca8575946227ed64c68e919988e03cec6
HussainiMagaji <43276126+HussainiMagaji@users.noreply.github.com>
2024-08-09 11:10:08 +0100

    Add files via upload

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,1 +1,22 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
 
+contract Counter {
+    uint private count;
+
+    constructor(uint _count) {
+        count = _count;
+    }
+
+    function get( ) public view returns (uint) {
+        return count;
+    }
+
+    function inc( ) public {
+        count += 1;
+    }
+
+    function dec( ) public {
+        count -= 1;
+    }
+}
